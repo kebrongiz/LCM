ecf3172e314ffa645be62ccd4a280a54920e96f8
kebron EL <kmamegiza@gmail.com>
2023-04-13 09:36:58 +0300

    # math calculation

---
+++ GCD.py
@@ -0,0 +1,31 @@
+def GCD(num1, num2):
+    num1_div = find_divisor(num1)
+    num2_div = find_divisor(num2)
+
+    if len(num1_div) < len(num2_div):
+        num1_div.reverse()
+        for val in num1_div:
+            if num2_div.count(val) > 0:
+                return val
+    else:
+        num2_div.reverse()
+        for val in num2_div:
+            if num1_div.count(val) > 0:
+                return val
+
+    return 1
+
+
+def find_divisor(num):
+    div = []
+    for val in range(1, num + 1):
+        if num % val == 0:
+            div.append(val)
+    return div
+
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+
+print("The GCD of {} and {} is {}".format(num1, num2, GCD(num1, num2)))
